b590d058d21fbaec0ce1dca93b37fbe6dbcd4966
Janhavitripathi <95627496+Janhavitripathi@users.noreply.github.com>
2021-12-16 11:27:17 +0530

    Add files via upload

---
+++ firstpro.cpp
@@ -0,0 +1,17 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    int num1,num2;
+cout<<"enter value of num1";
+cin>>num1;
+
+cout<<"enter the value of num2";
+cin>>num2;
+
+cout<<"sum"<<num1+num2;
+
+    return 0;
+}
+
