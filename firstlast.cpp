a73f4c368a0dde5aeebbad230404b05733277cb0
Janhavitripathi <95627496+Janhavitripathi@users.noreply.github.com>
2021-12-27 18:12:42 +0530

    Add files via upload

---
+++ firstlast.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+int num ;
+cin>>num;
+int count=0;
+while(num>0)
+{
+num=num/10;
+count++;
+
+}
+cout<<count<<endl;
+return 0;
+}
+
+
+
+
+
+
+
+
+
+
+
+
