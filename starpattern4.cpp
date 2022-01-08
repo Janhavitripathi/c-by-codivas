bde9e13ca1c9c9cbaa135706decd6bf1bf409d5d
Janhavitripathi <95627496+Janhavitripathi@users.noreply.github.com>
2022-01-08 10:43:47 +0530

    Add files via upload

---
+++ starpattern4.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int i,j,k;
+for(i=1;i<=5;i++)
+{
+    k=1;
+    for(j=1;j<=5;j++)
+    {
+if(j<=i)
+{
+    cout<<k;
+    k++;
+}
+else {
+    cout<<" ";
+}
+
+    }
+    cout<<endl;
+}
+return 0;
+}
