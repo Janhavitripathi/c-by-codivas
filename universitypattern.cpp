ba03eaf9ad384c69a1cb551cdf504cfcf3484777
Janhavi Tripathi <janhavitripathi2003@gmail.com>
2022-02-03 13:06:19 +0530

    Add files via upload

---
+++ universitypattern.cpp
@@ -0,0 +1,27 @@
+#include<bits/stdc++.h>
+#include<string>
+using namespace std;
+int main()
+{
+    int i,j;
+    char array[11]="UNIVERSITY";
+     for(i=1;i<=5;i++)
+     {
+         for(j=1;j<=2i;j++)
+         {
+             cout<<array[j-1];
+         }
+         cout<<endl;
+     }
+     int k=4;
+     for(i=6;i<=9;i++)
+     {   
+        for(j=1;j<=i*k;j++)
+        {
+         cout<<array[j-1];
+        } 
+        cout<<endl;
+        k--;
+     }
+    return 0;
+}
